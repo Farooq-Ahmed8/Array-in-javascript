f13818f03ad8fc7c4d6cb0fb38f31730b45785fd
Farooq Ahmed <03174748082ali@gmail.com>
2025-06-26 06:18:17 -0700

    array ended

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
-let fruits = 
-["Apple", "Banana", "Mango", "Kiwi", "Graphs",
- "Strawberry", "Watermaloon", "Orang", "Pineapple"];
+let fruits = ["Apple", "Banana", "Mango", "Kiwi", "Graphs", "Strawberry", "Watermaloon", "Orang", "Pineapple"];
 
  console.log(fruits);
+ 
+//let newArray = fruits.push("Dates");     //add  end
+//let newArray = fruits.pop();      // remove start 
+//let newArray = fruits.shift();    // remove start
+//let newArray = fruits.unshift();    // add end
+//let newArray = fruits.sort();    // line by line (e.g) A B C D E F || 1 2 3 4 5 6
+//let newArray = fruits.includes("Apple");    // item is availble (true)
+//let newArray = fruits.includes("X Y Z");      // item is not availble (false)
+//let newArray = fruits.join("A ");     // join in items after
+//let newArray = fruits.concat();     //concatenate to Array
+//let newArray = fruits.slice(2, 5);    // slice part of array
+let newArray = fruits.splice(1, 0, "XYZ",)    //  new item add before or after item index 
+
+
+//console.log(newArray);
+ 
+ 
